2ab8447795c5e70eb0475b3904b00b7b2b710308
anasenguady <74250696+anasenguady@users.noreply.github.com>
2020-11-10 18:15:42 +0100

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macbook
+ */
+public class NewClass {
+    //ceci est ma classe
+    
+}
+
+public Zonegeographique getSecteur() {
+        //T0D0
+        return secteur ;
+}
